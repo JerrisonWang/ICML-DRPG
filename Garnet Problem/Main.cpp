c8713cde8072ad0d5e8a27c51c7ddc8fbeeceb06
Qiuhao Wang <124043088+JerrisonWang@users.noreply.github.com>
2023-05-22 15:50:11 +0800

    DRPG-Garnet

---
+++ Garnet Problem/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Sampling Garnet.h"
+
+using namespace std;
+
+int main() {
+	Sample_sa(5, 6, 3, 200, 0.05, 0.1, 50);
+	//Sample_sa(10, 6, 3, 200, 0.01, 0.1, 50);
+	//Sample_sa(15, 6, 3, 200, 0.01, 0.1, 50);
+	system("pause");
+	return 0;
+}
